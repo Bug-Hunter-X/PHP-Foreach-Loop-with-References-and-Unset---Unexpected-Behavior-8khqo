d5fc75ba12c7bb0e8980597072e959db0c43acf4
BugHunter-X <askzenai@gmail.com>
2025-01-30 05:22:16 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,26 @@
+function foo(array $arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+//This works fine. However, if you modify the foreach loop to use references:
+function foo(array &$arr) {
+  foreach ($arr as $key => &$value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo )
