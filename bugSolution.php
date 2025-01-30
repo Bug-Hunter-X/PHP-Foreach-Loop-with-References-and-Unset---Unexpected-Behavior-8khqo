b5150d65bf75e5aa12d1888d3cbce968c2175e4a
BugHunter-X <askzenai@gmail.com>
2025-01-30 05:22:16 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,25 @@
+function foo(array $arr) {
+  $keysToRemove = [];
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      $keysToRemove[] = $key;
+    }
+  }
+  foreach ($keysToRemove as $key) {
+    unset($arr[$key]);
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+//Alternative solution using array_filter
+function foo(array $arr) {
+  return array_filter($arr, function ($value) { return $value !== 'bar'; });
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
